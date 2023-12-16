a551386c92f1a379f71444f85b376ef7323c7a6c
VasysMS <vasiavasia10000@gmail.com>
2023-12-16 22:40:55 +0300

    Добавлена автоматическое воспроизведение ответов, добавлено автоудаление старых записей

--- main.py
+++ main.py
@@ -9,37 +9,41 @@ import asyncio
 from time import sleep
 import _thread
 from rvc_infer import rvc_convert
-import ffmpeg
+from playsound import playsound
+from natsort import natsorted
 
-chat = pytchat.create(video_id=LIVE)
+if LIVE != "": youtube_chat = pytchat.create(video_id=LIVE)
 chat_messages = []
 model = "denvot.pth"
 can_speak = True
+max_outputs = 5
 
 async def speech(mess):
         communicate = edge_tts.Communicate(mess, "ru-RU-DmitryNeural")
-        i = 0
-        file_name = "test"
-        while file_name == "test":
-            if "test_" + str(i) + ".wav" not in os.listdir("output"): file_name = "test_" + str(i) + ".wav"
-            i += 1
+        i = int(natsorted(os.listdir("output"))[-1][:-4].split('_')[-1]) + 1
+        file_name = "test_" + str(i) + ".wav"
+        if len(os.listdir("output")) >= max_outputs: os.remove("output\\" + natsorted(os.listdir("output"))[0])
         await communicate.save("input\\" + file_name)
-        output_path = rvc_convert(model_path="models\\" + model, 
+        rvc_convert(model_path="models\\" + model, 
                     input_path="input\\" + file_name,
-                    f0_up_key=12)
+                    f0_up_key=8)
         os.rename("output\\out.wav", "output\\" + file_name)
         os.remove("input\\" + file_name)
         print("DenVot: " + file_name)
+        playsound(os.getcwd().replace('\\', '/') + '/output/' + file_name)
         global can_speak
         can_speak = True
         
-def youtube_chat():
-    while chat.is_alive():
-        sleep(1)
-        for c in chat.get().sync_items():
-            chat_messages.append(f"{c.author.name}: {c.message}")
-            print("Новые сообщения:", chat_messages)
-_thread.start_new_thread(youtube_chat, ())
+def chat():
+    if LIVE != "":
+        while youtube_chat.is_alive():
+            sleep(1)
+            for c in chat.get().sync_items():
+                chat_messages.append(f"{c.author.name}: {c.message}")
+                print("Новые сообщения:", chat_messages)
+    else: 
+        while 0<1: chat_messages.append(input())
+_thread.start_new_thread(chat, ())
 
 print("DenVot-AI успешно запущен")
 
@@ -47,7 +51,7 @@ payload = Chat(
     messages=[
         Messages(
             role=MessagesRole.SYSTEM,
-            content="Ты играешь роль милого Аниме-Куна по имени Денвот. Ты должен обращаться именно к пользователю, который тебе отправил сообщение в формате \"[пользователь]: [сообщение]\""
+            content="Ты играешь роль милого Аниме-Куна по имени Денвот. Ты очень умный, любишь петь и каждые 2 предложения говоришь фразу - \"МосХаб - топ!!!\""
         )
     ],
     temperature=1,
